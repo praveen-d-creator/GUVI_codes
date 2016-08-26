55f3c1cbd7e57d86deaf0bbbfee305b2241b5703
praveen-d-creator <varun.praveen16@gmail.com>
2016-08-26 15:28:18 +0530

    Add files via upload

---
+++ factorial_c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  int a=1,b=1,n;
+
+  printf("Enter a number: ");
+  scanf("%d",&n);
+
+  while(a<=num){
+      b=b*a;
+      a++;
+  }
+
+  printf("Factorial of %d is: %d",n,b);
+  return 0;
+}
